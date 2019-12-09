d65325d3c048c5fcbc63428e83f2063e9d50db0b
Tony Milone <tonymilone.tech@gmail.com>
2019-12-09 00:25:28 -0600

    50-limit

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
-function getDogImages(num=3){
-    fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
-    .then(response => response.json())
-    .then(responseJson =>
-      displayImages(responseJson,num))
-    .catch(error => 'Something went wrong! Try again later!')
-  }
-  
-  function displayImages(responseJson, x){
-  console.log(responseJson)
-  $('.results').html('');
-  for(let i=0;i<x;i++){
-  $('.results').append(`<img src='${responseJson.message[i]}' class='img-results' alt='random dogs'>`)
-  
-  $('.results').removeClass('hidden');
-    }
-  }
-  
-  function formWatch (){
-    $('form').submit(event =>{
-      event.preventDefault();
-      let x = $(`input[class='num']`).val();
-      getDogImages(x);
-    });
-  }
-  
-function appRunner(){
-    console.log('App is loaded, waiting on submit')
-    formWatch();
+function getDogImage(num = 3){
+  fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
+  .then(response => response.json())
+  .then(responseJson =>
+      displayResults(responseJson, num))
+  .catch(error => alert('not right!'));
+
+
 }
 
-$(appRunner);
+function displayResults(responseJson, x) {
+  console.log(responseJson);
+  //replace the existing image with the new one
+  $('.results').html('')
+  for(let i = 0; i < x; i ++ ) {
+  $('.results').append(
+    `<img src="${responseJson.message[i]}" class="imgs-results" alt = "random dogs">`
+  )
+  }
+  //display the results section
+  $('.results').removeClass('hidden');
+}
+
+
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    let x = $(`input[class = "inp"`).val();
+
+    if( x > 50){
+      x = 50
+    };
+    
+    getDogImage(x); 
+  });
+}
+
+function hold() {
+   
+    $(watchForm);
+}
+
+$(hold);
